f10d30ff225874acce8dc3ff9f062f5148f785f3
Cebotari Anastasia <71016019+anastasiacebotari15@users.noreply.github.com>
2020-10-26 12:06:26 +0300

    Delete while1.py

--- while1.py
+++
@@ -1,8 +0,0 @@
-"""Se introduc succesiv numere nenule până la introducerea numărului 0. Să se afişeze suma 
-tuturor numerelor introduse."""
-suma=0
-nr=1
-while nr!=0 :
-    nr=eval(input("Introduceti un numar pozitiv:"))
-    suma+=nr 
-print(suma)
